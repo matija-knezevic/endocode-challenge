4bcd8b356def71ef8c4bc701aff73d053cb7440a
Matija Knezevic <matija.knezevic.x@gmail.com>
2022-08-07 00:01:30 +0200

    Add second endpoint

--- http-service.py
+++ http-service.py
@@ -1,8 +1,19 @@
+from crypt import methods
 from flask import Flask
+from flask import request
+import re
+
 app = Flask(__name__)
 
-
-@app.route("/helloworld")
+# First endpoint
+@app.route("/helloworld")   
 
 def hello_world():
-    return "Hello Stranger"
+    
+    name = request.args.get("name")  
+    
+    if name is None:
+        return "Hello Stranger"
+    else:    
+        cut_case = re.sub(r'(?<!^)(?=[A-Z])', ' ', name)
+        return str(cut_case)
