ce46541e5993842b5b14eeb57a6f24ae0cc60dba
Matija Knezevic <matija.knezevic.x@gmail.com>
2022-08-08 16:10:40 +0200

    Add logging

--- http-service.py
+++ http-service.py
@@ -6,6 +6,7 @@ from urllib.request import urlopen
 import re
 import json
 import urllib
+import logging
 
 
 app = Flask(__name__)
@@ -21,7 +22,7 @@ def hello_world():
     if name is None:
         return "Hello Stranger"
     else:    
-        cut_case = re.sub(r'(?<!^)(?=[A-Z])', ' ', name)
+        cut_case = re.sub(r"(?<!^)(?=[A-Z])", " ", name)
         return str(cut_case)
 
 # versionz endpoint
@@ -35,12 +36,14 @@ def get_git_stuff():
         url,
         data=None,
         headers={
-            'User-Agent': 'endocode-challenge by github.com/matija-knezevic/endocode-challenge'
+            "User-Agent": "endocode-challenge by github.com/matija-knezevic/endocode-challenge"
         }
     )
     response = urllib.request.urlopen(request)
-    return json.loads(response.read().decode('utf-8')) 
+    return json.loads(response.read().decode("utf-8")) 
 
-
-
-
+# Logging
+logging.basicConfig(
+        format='%(asctime)s %(levelname)-8s %(message)s',
+        level=logging.INFO,
+        datefmt='%Y-%m-%d %H:%M:%S')
